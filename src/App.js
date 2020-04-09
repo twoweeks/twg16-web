de7cd39a78361e241f4dac7281869b811e0bcf2e
Dmitry <dmitry@cojam.ru>
2020-04-09 17:08:39 +0500

    initial

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = props => {
+    return <div className="app">Здесь что-то будет</div>;
+};
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,8 @@
+const config = {
+    host: 'http://localhost:1337/request.php',
+    api_keys: {
+        recaptcha: '',
+    },
+};
+
+export default config;
